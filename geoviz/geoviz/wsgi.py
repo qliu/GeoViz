084525dfcb3597e05b4ace367ba66020a1cb5b07
Qing Liu <Qing.Liu@du.edu>
2016-08-07 00:50:03 -0600

    change wsgi

--- geoviz/geoviz/wsgi.py
+++ geoviz/geoviz/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
-
-from django.core.wsgi import get_wsgi_application
-
+import os, sys
+sys.path.append('/django-apps/geoviz/geoviz/')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/django-apps/geoviz/geoviz/egg_cache")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geoviz.settings")
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
